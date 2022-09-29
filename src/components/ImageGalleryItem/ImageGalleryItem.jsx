88f83b99c89151adba6a7c62679cf2b8e397e272
Darya <daryabobur@gmail.com>
2022-09-29 21:58:26 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,13 +1,34 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { ImageGalleryCard, Image } from './ImageGalleryItemStyled';
+import Modal from 'components/Modal/Modal';
 
-const ImageGalleryItem = ( { items, onClick } ) => {
+const ImageGalleryItem = ({ items }) => {
+
+  const [openModal, setOpenModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [tags, setTags] = useState('');
+
+  const isOpenModal = ({ largeImageURL, tags }) => {
+    setOpenModal(true);
+    setLargeImageURL(largeImageURL);
+    setTags(tags);
+  }
+
   return (
-    items.map(({id, webformatURL, tags, largeImageURL}) => (
-      <ImageGalleryCard key={id} onClick={() => onClick({largeImageURL, tags})}>
-        <Image src={webformatURL} alt={tags}/>
-      </ImageGalleryCard>
-    ))
+    <>
+      {items.map(({id, webformatURL, tags, largeImageURL}) => (
+        <ImageGalleryCard key={id} onClick={() => isOpenModal({ largeImageURL, tags })}>
+          <Image src={webformatURL} alt={tags}/>     
+        </ImageGalleryCard>
+      ))}
+
+      {openModal && 
+        <Modal onClose={() => setOpenModal(false)}>
+          <img src={largeImageURL} alt={tags}/>
+        </Modal>
+      }
+    </> 
   )
 }
 
@@ -15,8 +36,8 @@ ImageGalleryItem.propTypes = {
   items: PropTypes.array,
   id: PropTypes.number,
   webformatURL: PropTypes.string,
+  largeImageURL: PropTypes.string,
   tags: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
 }
 
 export default ImageGalleryItem; 
